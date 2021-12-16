9d334c34cb6c697234924ac7dcf8aaa81a31d574
justinksw <78139499+justinksw@users.noreply.github.com>
2021-12-16 17:56:57 +0800

    Add files via upload

---
+++ js/luckydraw.js
@@ -0,0 +1,12 @@
+function drawNext() {
+  var lastdraw = $("#luckyperson").html();
+  var alllucky = $("#drawhistory").html();
+  
+  $("#drawhistory").html(lastdraw + '<br />' + alllucky);
+  
+  var personname = data[Math.floor(Math.random() * data.length)];
+  
+  $("#luckyperson").html(personname);
+}
+
+
